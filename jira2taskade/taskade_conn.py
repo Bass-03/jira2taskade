735513323d8509107b45d62a9a0fdbb04aef1cc7
Edmundo Sanchez <zomundo@gmail.com>
2024-08-13 23:13:04 -0600

    fix: Adjust taskade conn to work on reallife
    Needed to adjust the mardown and the placement

--- jira2taskade/taskade_conn.py
+++ jira2taskade/taskade_conn.py
@@ -4,23 +4,24 @@ import requests
 class TaskadeConn:
     def __init__(self, token):
         self.headers = {
+            "accept": "application/json",
             "Content-Type": "application/json",
             "Authorization": f"Bearer {token}"
         }
 
     def get_tasks(self, project_id):
-        url = f"https://taskade.com/api/v1/projects/{project_id}/tasks"
+        url = f"https://www.taskade.com/api/v1/projects/{project_id}/tasks"
         tasks = requests.get(url, headers=self.headers)
         return tasks.json()
 
     def create_task(self, project_id, task_content):
-        url = f"https://taskade.com/api/v1/projects/{project_id}/tasks"
+        url = f"https://www.taskade.com/api/v1/projects/{project_id}/tasks"
         data = {
             "tasks": [
                 {
                     "contentType": "text/markdown",
-                    "content": "text",
-                    "placement": "afterbegin"
+                    "content": f" - [ ] {task_content} #jira",
+                    "placement": "beforeend"
                 }
             ]
         }
