2b7e0ad06e62721803b1a2ad5620ad8e8b3860f3
Edmundo Sanchez <zomundo@gmail.com>
2024-08-13 19:55:59 -0600

    chore: Implement new config reader on the Jira connection

--- jira2taskade/jira_conn.py
+++ jira2taskade/jira_conn.py
@@ -1,18 +1,13 @@
-import json
 from jira import JIRA
 
 
 class JiraConn:
-    def __init__(self, config_file):
-        # load config
-        with open(config_file, "r") as f:
-            config = json.load(f)
+    def __init__(self, server, email, token):
         self.jira = JIRA(
-            server=config["jira_server"],
-            basic_auth=(config["api_email"], config["api_token"]),
+            server=server,
+            basic_auth=(email, token),
         )
-        self.jql = config["jql"]
 
-    def get_issues(self):
-        issues = self.jira.search_issues(self.jql)
+    def get_issues(self, jql):
+        issues = self.jira.search_issues(jql)
         return issues
--- tests/test_jira_conn.py
+++ tests/test_jira_conn.py
@@ -1,55 +1,56 @@
 import os
-from jira2taskade.jira_conn import JiraConn
+import pytest
 from unittest import mock
+from jira2taskade.jira_conn import JiraConn
+from jira2taskade.config_read import ReadConfig
 
 
-def test_init():
+@pytest.fixture
+def config():
+    test_dir = os.path.dirname(os.path.abspath(__file__))
+    config_file = os.path.join(test_dir, "test_config.json")
+    return ReadConfig(config_file)
+
+
+def test_init(config):
     with mock.patch("jira2taskade.jira_conn.JIRA") as mock_jira:
-        test_dir = os.path.dirname(os.path.abspath(__file__))
-        config_file = os.path.join(test_dir, "test_config.json")
-        jira = JiraConn(config_file)
+        jira = JiraConn(config.jira_server, config.jira_email, config.jira_token)
         # Perform assertions or further testing with the mocked JIRA object
         assert mock_jira.called  # Check if the JIRA constructor was called
         assert mock_jira.call_args == mock.call(
-            server="https://fake.atlassian.net/",
-            basic_auth=("fake@fake.com", "fakeToken"),
+            server=config.jira_server,
+            basic_auth=(config.jira_email, config.jira_token),
         )  # Check if the JIRA constructor was called with the correct arguments
 
         assert jira.jira == mock_jira.return_value
 
 
-def test_get_issues():
+def test_get_issues(config):
     with mock.patch("jira2taskade.jira_conn.JIRA"):
-        test_dir = os.path.dirname(os.path.abspath(__file__))
-        config_file = os.path.join(test_dir, "test_config.json")
-        jira = JiraConn(config_file)
-
+        jira = JiraConn(config.jira_server, config.jira_email, config.jira_token)
         # Mock the return value of get_issues
         mock_issues = mock.MagicMock()
         mock_issues.return_value = ["ISSUE-1", "ISSUE-2", "ISSUE-3"]
         jira.jira.search_issues = mock_issues
 
         # Call the get_issues function
-        issues = jira.get_issues()
+        issues = jira.get_issues(config.jql)
 
         # Perform assertions
         assert mock_issues.called
         assert issues == ["ISSUE-1", "ISSUE-2", "ISSUE-3"]
 
 
-def test_get_issues_empty():
+def test_get_issues_empty(config):
     with mock.patch("jira2taskade.jira_conn.JIRA"):
-        test_dir = os.path.dirname(os.path.abspath(__file__))
-        config_file = os.path.join(test_dir, "test_config.json")
-        jira = JiraConn(config_file)
-
+        jira = JiraConn(config.jira_server, config.jira_email, config.jira_token)
         # Mock the return value of get_issues to be an empty list
         mock_issues = mock.MagicMock()
         mock_issues.return_value = []
         jira.jira.search_issues = mock_issues
 
         # Call the get_issues function
-        issues = jira.get_issues()
+        issues = jira.get_issues(config.jql)
 
         # Perform assertions
         assert mock_issues.called
