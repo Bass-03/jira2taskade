725a39048fe1be97726775996a57d166fb31a051
Edmundo Sanchez <zomundo@gmail.com>
2024-08-13 19:55:30 -0600

    feat; Implement new class to read config

---
+++ tests/test_config_read.py
@@ -0,0 +1,19 @@
+import os
+from jira2taskade.config_read import ReadConfig
+
+
+def test_read_config():
+    test_dir = os.path.dirname(os.path.abspath(__file__))
+    config_file = os.path.join(test_dir, "test_config.json")
+    # Call the read_config function
+    config = ReadConfig(config_file)
+    # Perform assertions
+    config_data = {
+        "jira_server": "https://fake.atlassian.net/",
+        "jira_email": "fake@fake.com",
+        "jira_token": "fakeToken",
+        "jql": "assignee = currentUser() and sprint in openSprints()"
+    }
+    print(config.jira_server)
+    assert config.jira_server == config_data["jira_server"]
+    assert config.jql == config_data["jql"]
