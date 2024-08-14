4ee76bf5548858b839dcb8dc783a6afb81a7f4c1
Edmundo Sanchez <zomundo@gmail.com>
2024-08-13 20:31:27 -0600

    fix: Correct returns from requests to taskade

--- jira2taskade/taskade_conn.py
+++ jira2taskade/taskade_conn.py
@@ -3,15 +3,18 @@ import requests
 
 class TaskadeConn:
     def __init__(self, token):
-        self.headers = {"Authorization": f"Bearer {token}"}
+        self.headers = {
+            "Content-Type": "application/json",
+            "Authorization": f"Bearer {token}"
+        }
 
     def get_tasks(self, project_id):
-        url = f"https://api.taskade.com/v1/projects/{project_id}/tasks"
+        url = f"https://taskade.com/api/v1/projects/{project_id}/tasks"
         tasks = requests.get(url, headers=self.headers)
-        return tasks.content
+        return tasks.json()
 
     def create_task(self, project_id, task_content):
-        url = f"https://api.taskade.com/v1/projects/{project_id}/tasks"
+        url = f"https://taskade.com/api/v1/projects/{project_id}/tasks"
         data = {
             "tasks": [
                 {
@@ -21,6 +24,6 @@ class TaskadeConn:
                 }
             ]
         }
-        req = requests.post(url, headers=self.headers, json=data)
-        req.raise_for_status()
-        return req.content
+        task = requests.post(url, headers=self.headers, json=data)
+        task.raise_for_status()
+        return task.json()
--- tests/test_taskade_conn.py
+++ tests/test_taskade_conn.py
@@ -12,16 +12,11 @@ def config():
     return ReadConfig(config_file)
 
 
-@pytest.fixture
-def conn():
-    return TaskadeConn("test_token")
-
-
 def test_get_tasks(config):
     with mock.patch("jira2taskade.taskade_conn.requests.get") as mock_get:
         taskade = TaskadeConn(config.taskade_token)
         mock_response = mock.Mock()
-        mock_response.content = {"tasks": ["task1", "task2", "task3"]}
+        mock_response.json.return_value = {"tasks": ["task1", "task2", "task3"]}
         mock_get.return_value = mock_response
 
         tasks = taskade.get_tasks("project_id")
@@ -34,7 +29,7 @@ def test_create_task(config):
     with mock.patch("jira2taskade.taskade_conn.requests.post") as mock_post:
         taskade = TaskadeConn(config.taskade_token)
         mock_response = mock.Mock()
-        mock_response.content = {"task_id": "123"}
+        mock_response.json.return_value = {"task_id": "123"}
         mock_post.return_value = mock_response
 
         task = taskade.create_task("project_id", "task_content")
